550a7a9f91206c7ee43f4e6390f51e600d4d251d
maorzehavi <maorzehavi@outlook.com>
2023-05-08 18:33:57 +0300

    adding customer-service

--- category-service/src/main/java/com/maorzehavi/categoryservice/CategoryServiceApplication.java
+++ category-service/src/main/java/com/maorzehavi/categoryservice/CategoryServiceApplication.java
@@ -1,8 +1,13 @@
 package com.maorzehavi.categoryservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
 
 @SpringBootApplication
 @EnableDiscoveryClient
